ab269e4d314a6a3a644c390f0236d3ce36a80e91
NathanNdjoni <brakiplace@gmail.com>
2019-02-20 12:11:45 +0100

    premier js fini Changecolor

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,60 @@ $('#myCarousel').carousel({
       next.children(':first-child').clone().appendTo($(this));
     }
   });
+
+
+// changeColor
+
+let btnColorChangeW = document.querySelector('#blanc')
+let btnColorChangeB = document.querySelector('#noir')
+
+let elements = document.querySelectorAll(".theme");
+
+const changeWhite = () => {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style[elements[i].dataset.prop] = "";
+    }
+};
+
+btnColorChangeW.addEventListener('click',changeWhite)
+
+const changeBlack = () => {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style[elements[i].dataset.prop] = elements[i].dataset.kleur;
+    }
+};
+
+btnColorChangeB.addEventListener('click',changeBlack)
+
+// Create Modale 
+
+
+let Modale = document.getElementById('myModal')
+let btnConnection = document.querySelector('#btn-connection')
+let spanCroix = document.getElementsByClassName("close")[0]
+console.log(btnConnection)
+console.log(Modale)
+console.log(spanCroix)
+
+const btnOneClick = () => {
+    Modale.style.display = "block"
+    //Modale.classList.remove("d-none")
+
+}
+// Quand on appuie sur le button connecté , le modale apparait il apparait parce que on à mis le modal en display none 
+
+const spanOneClick = () => {
+    Modale.style.display = "none"
+}
+// Quand  appuie sur le cursor , le modale disparait parce qu'il est mis en display none
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
+
+
+btnConnection.addEventListener('click',btnOneClick)
+
+spanCroix.addEventListener('click',spanOneClick)
